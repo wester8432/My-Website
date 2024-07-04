d5248d9ce09320f373e9d543b6c32540d919609b
DESKTOP-SDJVT54\N1ro <wester8432@gmail.com>
2024-07-04 11:36:06 +0800

    Add express and axios dependencies

--- riot-api-proxy/index.js
+++ riot-api-proxy/index.js
@@ -7,14 +7,12 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// 使用 CORS 中间件
 app.use(cors());
 
-// 代理 Riot API 的请求
 app.get("/riot/:region/*", async (req, res) => {
   const region = req.params.region;
   const apiUrl = req.params[0];
-  const apiKey = process.env.RIOT_API_KEY; // 请确保在环境变量中设置了 RIOT_API_KEY
+  const apiKey = process.env.RIOT_API_KEY;
 
   try {
     const response = await axios.get(
